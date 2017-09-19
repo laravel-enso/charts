858549d131b7697ad9934e144e6f35b48540104f
Mihai <mihai.ocneanu@gmail.com>
2017-09-19 13:48:35 +0300

    fixed versions. Moved VueJS components to the vueadminlte package

--- src/ChartsServiceProvider.php
+++ src/ChartsServiceProvider.php
@@ -17,14 +17,6 @@ class ChartsServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/config' => config_path(),
         ], 'enso-config');
-
-        $this->publishes([
-            __DIR__.'/resources/assets/js/components' => resource_path('assets/js/vendor/laravel-enso/components'),
-        ], 'charts-component');
-
-        $this->publishes([
-            __DIR__.'/resources/assets/js/components' => resource_path('assets/js/vendor/laravel-enso/components'),
-        ], 'enso-update');
     }
 
     public function register()
