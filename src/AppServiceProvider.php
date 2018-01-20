622e238a8fd0e003d8097ae34623c4fc866f6709
mishu <mishu@xtelecom.ro>
2018-01-20 08:53:24 +0200

    integrates the vue components

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -17,6 +17,14 @@ class AppServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/config' => config_path('enso'),
         ], 'enso-config');
+
+        $this->publishes([
+            __DIR__.'/resources/assets/js' => resource_path('assets/js'),
+        ], 'charts-assets');
+
+        $this->publishes([
+            __DIR__.'/resources/assets/js' => resource_path('assets/js'),
+        ], 'enso-assets');
     }
 
     public function register()
