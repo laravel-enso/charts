0c96a398ca3028a20ad0ce47b199260b391e1d77
reza <raftx24@gmail.com>
2020-06-30 19:59:27 +0430

    - fixes directories

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -11,11 +11,7 @@ class AppServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(__DIR__.'/../config/charts.php', 'enso.charts');
 
         $this->publishes([
-            __DIR__.'/../config ' => config_path('enso'),
-        ], 'charts-config');
-
-        $this->publishes([
-            __DIR__.'/../config ' => config_path('enso'),
-        ], 'enso-config');
+            __DIR__.'/../config' => config_path('enso'),
+        ], ['charts-config', 'enso-config']);
     }
 }
